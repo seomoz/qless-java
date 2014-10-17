98775b19ec4bd6a36c6ed34d98d9d0280e364a5e
John Zhu <john.zhu@moz.com>
2014-10-16 18:01:20 -0700

    Jobs implementation and its test

---
+++ qless-java/src/test/java/com/moz/qless/client/QueuesTest.java
@@ -0,0 +1,22 @@
+package com.moz.qless.client;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class QueuesTest {
+
+  @Test
+  @Ignore
+  public void basic() {
+  }
+
+  @Test
+  @Ignore
+  public void counts() {
+  }
+
+  @Test
+  @Ignore
+  public void attributeError() {
+  }
+}
