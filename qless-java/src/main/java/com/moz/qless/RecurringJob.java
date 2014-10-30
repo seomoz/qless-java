375feb11caa61630041b293e53a428ff9c4bc660
John Zhu <john.zhu@moz.com>
2014-10-30 11:51:36 -0700

    add test coverage to RecurringJob

--- qless-java/src/main/java/com/moz/qless/RecurringJob.java
+++ qless-java/src/main/java/com/moz/qless/RecurringJob.java
@@ -1,10 +1,196 @@
 package com.moz.qless;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-/**
- * A regular qless RecurringJob
- *
- */
+import com.moz.qless.lua.LuaCommand;
+import com.moz.qless.lua.LuaConfigParameter;
+import com.moz.qless.utils.JsonUtils;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.annotate.JacksonInject;
+
 public class RecurringJob extends Job {
 
+  @JsonProperty
+  protected int backlog;
+
+  @JsonProperty
+  protected int count;
+
+  @JsonProperty
+  protected int interval;
+
+  @JsonProperty
+  protected int retries;
+
+  @JsonCreator
+  RecurringJob(@JacksonInject("client") final Client client) {
+      super(client);
+  }
+
+  public int getBacklog() {
+      return this.backlog;
+  }
+
+  public void backlog(final int backlog) throws IOException {
+    this.client.call(
+        LuaCommand.RECUR_UPDATE.toString(),
+        this.jid,
+        LuaConfigParameter.BACKLOG.toString(),
+        Integer.toString(backlog));
+
+    this.backlog = backlog;
+  }
+
+  @Override
+  public void cancel() throws IOException {
+      this.client.call(
+          LuaCommand.UNRECUR.toString(),
+          this.jid);
+  }
+
+  public int getCount() {
+      return this.count;
+  }
+
+  public void data(final Map<String, Object> data) throws IOException {
+      this.client.call(
+          LuaCommand.RECUR_UPDATE.toString(),
+          this.jid,
+          LuaConfigParameter.DATA.toString(),
+          JsonUtils.stringify(data));
+
+      this.data = data;
+  }
+
+  @Override
+  public void setDataField(final String key, final Object value) throws IOException {
+    this.data.put(key,  value);
+    this.data(this.data);
+  }
+
+  public int getInterval() {
+      return this.interval;
+  }
+
+  public void interval(final int interval) {
+      this.interval = interval;
+  }
+
+  @Override
+  public boolean isRecurring() {
+    return true;
+  }
+
+  public double next() {
+    return this.client
+        .getJedisPool()
+        .getResource()
+        .zscore("ql:q:" + this.queueName + "-recur", this.jid);
+  }
+
+  public void klass(final String klass) throws IOException {
+      this.client.call(
+          LuaCommand.RECUR_UPDATE.toString(),
+          this.jid,
+          LuaConfigParameter.KLASS.toString(),
+          klass);
+
+      this.klass = klass;
+  }
+
+  public String lastSpawnedJid() {
+      if (this.neverSpawned()) {
+          return null;
+      }
+      return this.jid + "-" + this.count;
+  }
+
+  public Job lastSpawnedJob() throws IOException {
+      if (this.neverSpawned()) {
+          return null;
+      }
+      return this.client.getJobs().get(this.lastSpawnedJid());
+  }
+
+  @Override
+  public String move(final String queueName) throws IOException {
+    this.queueName = queueName;
+    this.queue = null;
+
+    final Object result = this.client.call(
+        LuaCommand.RECUR_UPDATE.toString(),
+        this.jid,
+        LuaConfigParameter.QUEUE.toString(),
+        queueName);
+
+    return result.toString();
+  }
+
+  public boolean neverSpawned() {
+      return 0 == this.count;
+  }
+
+  @Override
+  public void priority(final int priority) throws IOException {
+      this.client.call(
+          LuaCommand.RECUR_UPDATE.toString(),
+          this.jid,
+          LuaConfigParameter.PRIORITY.toString(),
+          Integer.toString(priority));
+
+      this.priority = priority;
+  }
+
+  @Override
+  public void requeue(final String queueName) throws IOException {
+      this.move(queueName);
+  }
+
+  @Override
+  public int getRetries() {
+      return this.retries;
+  }
+
+  public void retries(final int retries) throws IOException {
+      this.client.call(
+          LuaCommand.RECUR_UPDATE.toString(),
+          this.jid,
+          LuaConfigParameter.RETRIES.toString(),
+          Integer.toString(retries));
+
+      this.retries = retries;
+  }
+
+  @Override
+  public void tag(final String... tags) throws IOException {
+      final List<String> args = new ArrayList<String>();
+      args.add(this.jid);
+
+      for (final String tag: tags) {
+          args.add(tag);
+      }
+
+      this.client.call(
+          LuaCommand.RECUR_TAG.toString(),
+          args);
+  }
+
+  @Override
+  public void untag(final String... tags) throws IOException {
+      final List<String> args = new ArrayList<String>();
+      args.add(this.jid);
+
+      for (final String tag: tags) {
+          args.add(tag);
+      }
+
+      this.client.call(
+          LuaCommand.RECUR_UNTAG.toString(),
+          args);
+  }
 }
--- qless-java/src/test/java/com/moz/qless/RecurringJobTest.java
+++ qless-java/src/test/java/com/moz/qless/RecurringJobTest.java
@@ -1,64 +1,181 @@
 package com.moz.qless;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import com.moz.qless.client.ClientHelper;
+import com.moz.qless.lua.LuaConfigParameter;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import redis.clients.jedis.JedisPool;
+
 public class RecurringJobTest {
+  private final JedisPool jedisPool = new JedisPool(ClientHelper.DEFAULT_HOSTNAME);
+  private Client client;
+  private Queue queue;
+  private static final String DEFAULT_NAME = "foo";
 
-  @Test
-  public void attribute() {
-    Assert.fail("Not yet implemented");
+  @Before
+  public void before() throws IOException {
+    this.client = ClientCreation.create(this.jedisPool);
+    this.queue = new Queue(this.client, RecurringJobTest.DEFAULT_NAME);
   }
 
   @Test
-  public void setPriority() {
-    Assert.fail("Not yet implemented");
+  public void recurringJobAttributes() throws IOException {
+    final String jid = this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, null);
+    final RecurringJob job = (RecurringJob) this.client.getJobs().get(jid);
+
+    assertThat(job.getJid(),
+        equalTo(jid));
+    assertThat(job.getInterval(),
+        equalTo(60));
+    assertThat(job.getKlassName(),
+        equalTo(RecurringJobTest.DEFAULT_NAME));
+    assertThat(job.getQueueName(),
+        equalTo(RecurringJobTest.DEFAULT_NAME));
+    assertThat(job.getPriority(),
+        equalTo(Integer.parseInt(ClientHelper.DEFAULT_PRIORITY)));
+    assertThat(job.getRetries(),
+        equalTo(Integer.parseInt(ClientHelper.DEFAULT_RETRIES)));
   }
 
   @Test
-  public void setRetries() {
-    Assert.fail("Not yet implemented");
+  public void setPriority() throws IOException {
+    final Map<String, Object> opts = new HashMap<>();
+    final String jid = ClientHelper.generateJid();
+    opts.put("jid", jid);
+    opts.put(LuaConfigParameter.PRIORITY.toString(), 0);
+
+    this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, opts);
+    assertThat(this.client.getJobs().get(jid).priority,
+        equalTo(0));
+
+    this.client.getJobs().get(jid).priority(10);
+    assertThat(this.client.getJobs().get(jid).priority,
+        equalTo(10));
   }
 
   @Test
-  public void setInterval() {
-    Assert.fail("Not yet implemented");
+  public void setRetries() throws IOException {
+    final Map<String, Object> opts = new HashMap<>();
+    final String jid = ClientHelper.generateJid();
+    opts.put("jid", jid);
+    opts.put(LuaConfigParameter.RETRIES.toString(), 2);
+
+    this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, opts);
+    final RecurringJob job = (RecurringJob) this.client.getJobs().get(jid);
+    assertThat(job.getRetries(),
+        equalTo(2));
+
+    job.retries(10);
+    assertThat(this.client.getJobs().get(jid).getRetries(),
+        equalTo(10));
   }
 
   @Test
-  public void setData() {
-    Assert.fail("Not yet implemented");
+  public void setInterval() throws IOException {
+    final String jid = this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, null);
+    final RecurringJob job = (RecurringJob) this.client.getJobs().get(jid);
+
+    assertThat(job.getInterval(),
+        equalTo(60));
+
+    job.interval(100);
+    assertThat(job.getInterval(),
+        equalTo(100));
   }
 
   @Test
-  public void setKlass() {
-    Assert.fail("Not yet implemented");
+  public void setData() throws IOException {
+    final String jid = this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, null);
+    final RecurringJob job = (RecurringJob) this.client.getJobs().get(jid);
+
+    assertThat(job.getData().keySet(),
+        is(empty()));
+
+    final Map<String, Object> data = new HashMap<>();
+    data.put("key1", "value1");
+    data.put("key2", "value2");
+    job.data(data);
+
+    assertThat(job.getData().keySet(),
+        containsInAnyOrder("key1", "key2"));
   }
 
   @Test
-  public void getNext() {
-    Assert.fail("Not yet implemented");
+  public void setKlass() throws IOException {
+    final String jid = this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, null);
+    final RecurringJob job = (RecurringJob) this.client.getJobs().get(jid);
+
+    assertThat(job.getKlassName(),
+        equalTo(RecurringJobTest.DEFAULT_NAME));
+
+    job.klass("com.moz.qless.IntegrationTestJob");
+    assertThat(job.getKlassName(),
+        equalTo("com.moz.qless.IntegrationTestJob"));
   }
 
   @Test
-  public void attributeError() {
-    Assert.fail("Not yet implemented");
+  public void getNext() throws IOException {
+    final String jid = this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, null);
+    final RecurringJob job = (RecurringJob) this.client.getJobs().get(jid);
+
+    final double next = job.next();
+    this.queue.pop();
+
+    Assert.assertTrue((job.next() - next - 60) < 1);
   }
 
   @Test
-  public void move() {
-    Assert.fail("Not yet implemented");
+  public void move() throws IOException {
+    final Map<String, Object> data = new HashMap<>();
+    data.put("key", "value");
+
+    final Map<String, Object> opts = new HashMap<>();
+    opts.put(LuaConfigParameter.DEPENDS.toString(), Arrays.asList("jid2"));
+
+    final String jid = this.queue.recur(RecurringJobTest.DEFAULT_NAME, data, 60, opts);
+    final RecurringJob job = (RecurringJob) this.client.getJobs().get(jid);
+    assertThat(job.getJid(),
+        equalTo(jid));
+
+    job.move("bar");
+
+    assertThat(this.client.getJobs().get(jid).getQueueName(),
+        equalTo("bar"));
+    assertThat(this.client.getJobs().get(jid).getData().keySet(),
+        contains("key"));
   }
 
   @Test
-  public void cancel() {
-    Assert.fail("Not yet implemented");
+  public void cancel() throws IOException {
+    final String jid = this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, null);
+    final RecurringJob job = (RecurringJob) this.client.getJobs().get(jid);
+
+    job.cancel();
+    assertThat(this.client.getJobs().get(jid), nullValue());
   }
 
   @Test
-  public void tagUntag() {
-    Assert.fail("Not yet implemented");
+  public void tagUntag() throws IOException {
+    final String jid = this.queue.recur(RecurringJobTest.DEFAULT_NAME, null, 60, null);
+
+    this.client.getJobs().get(jid).tag(RecurringJobTest.DEFAULT_NAME);
+    assertThat(this.client.getJobs().get(jid).getTags(),
+        contains(RecurringJobTest.DEFAULT_NAME));
+
+    this.client.getJobs().get(jid).untag(RecurringJobTest.DEFAULT_NAME);
+    assertThat(this.client.getJobs().get(jid).getTags(),
+        is(empty()));
   }
+
 }
